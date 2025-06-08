97c86b99f0a3c17878680b85daf9b0f62a398721
Asif Islam <engrasifislam@gmail.com>
2025-06-08 19:57:12 +0600

    Add rest framework

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # External apps
+    'rest_framework',
+
     # Custom apps
     'users',
     'products',
