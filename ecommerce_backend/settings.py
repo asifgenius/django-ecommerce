d9f602bdc38ad20906428d381a609b98ba947a01
Asif Islam <engrasifislam@gmail.com>
2025-06-08 21:25:54 +0600

    Add REST_FRAMEWORK in settings

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -75,6 +75,19 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # or IsAuthenticated for auth-required APIs
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 
 
