937c02a5febc101d7fb3f091a45f0fe3bfb7311c
Asif Islam <engrasifislam@gmail.com>
2025-06-08 20:42:08 +0600

    product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    slug = models.SlugField(max_length=255, unique=True) 
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    sale_price = models.FloatField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='assets/product_images/')
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
