58e7aa251527ab8fb5b179dcc42065f2b849719b
Asif Islam <engrasifislam@gmail.com>
2025-06-09 01:36:22 +0600

    Add views and url for order app

---
+++ orders/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from orders.models import Order, OrderItem
+from products.models import Product
+from users.models import User
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        return order
+
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('items', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if items_data is not None:
+            instance.items.all().delete()
+            for item_data in items_data:
+                OrderItem.objects.create(order=instance, **item_data)
+
+        return instance
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+class OrderListCreateView(generics.ListCreateAPIView):
+    """
+    List all orders or create a new order with order items.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete an order and its items.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
