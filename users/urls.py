f98f6edbb6b1bc8c35fb1d58cf341c5e529c2cbe
Asif Islam <engrasifislam@gmail.com>
2025-06-09 14:39:07 +0600

    Fix token routes

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from .views import UserListAPIView
 
 urlpatterns = [
     # JWT token routes
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', UserListAPIView.as_view(), name='user-list'),
 ]
