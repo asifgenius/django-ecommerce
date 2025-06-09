07083c93821c647f90e603faabd83e7372e09e3d
Asif Islam <engrasifislam@gmail.com>
2025-06-09 12:07:12 +0600

    Add django filter for users

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import UserListAPIView
+
 urlpatterns = [
     # JWT token routes
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', UserListAPIView.as_view(), name='user-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import User
+from .serializers import UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+ 
 
 # Create your views here.
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['full_name', 'email', 'address']
